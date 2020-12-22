7f2fc353a99299f48bf8362e1fb2f5bc60bf04ab
jesuselias <jesus_e1992@hotmail.com>
2020-12-21 20:00:23 -0400

    finish layout

--- App.js
+++ App.js
@@ -1,114 +1,128 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { View, StyleSheet, Image, Text, ScrollView } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <ScrollView>
+    <View style={{ flexDirection: 'row' }}>
+      <Image 
+        style={styles.banner}
+        source={ require('./assets/img/bg.jpg') }/>
+    </View>
+
+    <View>
+      <Text style={styles.Titulo}>Que hacer en Paris</Text>
+      <ScrollView horizontal
+        style={{width:'100%', height:'15%'}}>
+        <View>
+          <Image 
+            style={styles.banner}
+            source={ require('./assets/img/actividad1.jpg') }/>
+        </View>
+        <View>
+          <Image 
+            style={styles.banner}
+            source={ require('./assets/img/actividad2.jpg') }/>
+        </View>
+        <View>
+          <Image 
+            style={styles.banner}
+            source={ require('./assets/img/actividad3.jpg') }/>
+        </View>
+        <View>
+          <Image 
+            style={styles.banner}
+            source={ require('./assets/img/actividad4.jpg') }/>
+        </View>
+        <View>
+          <Image 
+            style={styles.banner}
+            source={ require('./assets/img/actividad5.jpg') }/>
+        </View>
+      </ScrollView>
+
+      <Text style={styles.Titulo}>Los Mejores Alojamientos </Text>
+        <View>
+          <View>
+            <Image 
+              style={styles.mejores}
+              source={ require('./assets/img/mejores1.jpg') }/>
           </View>
-        </ScrollView>
-      </SafeAreaView>
+          <View>
+            <Image 
+              style={styles.mejores}
+              source={ require('./assets/img/mejores2.jpg') }/>
+          </View>
+          <View>
+            <Image 
+              style={styles.mejores}
+              source={ require('./assets/img/mejores3.jpg') }/>
+          </View>
+        </View>
+
+        <Text style={styles.Titulo}>Hospedaje en LA</Text>
+        <View style={styles.listado} >
+          <View style={styles.listadoItem}>
+            <Image 
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje1.jpg') }/>
+          </View>  
+          <View style={styles.listadoItem}>
+            <Image 
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje2.jpg') }/>
+          </View>
+          <View style={styles.listadoItem}>
+            <Image 
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje3.jpg') }/>
+          </View>
+          <View style={styles.listadoItem}>
+            <Image 
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje4.jpg') }/>
+          </View>
+        </View>
+
+        
+    </View>
+    </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  banner: {
+    height: 250,
+    flex: 1
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
+  Titulo: {
+    fontWeight: 'bold',
     fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+    marginVertical: 20
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  contenedor: {
+    marginHorizontal: 10
   },
-  highlight: {
-    fontWeight: '700',
+  ciudad: {
+    width:400,
+    height: 300,
+    marginRight: 10
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 7
   },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  listadoItem: {
+    flexBasis: '49%'
+  }  
 });
 
 export default App;
